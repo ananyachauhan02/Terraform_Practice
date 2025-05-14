e2f4af67488d499bb02b6197204e6fe3d639d5c5
ananya <ananya.chauhan@zypp.app>
2025-05-14 23:25:05 +0530

    Day-3

---
+++ DAY-3/modules/ec2-instance/terraform.tfvars
@@ -0,0 +1,6 @@
+// this file is used to store variables seperately
+
+ami_value = "ami-084568db4383264d4"
+instance_type_value = "t2.micro"
+
+
---
+++ DAY-3/modules/ec2-instance/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+    description = "value for ami"
+    default = " "
+
+}
+
+variable "instance_type_value" {
+    description = "value for instance"
+}
